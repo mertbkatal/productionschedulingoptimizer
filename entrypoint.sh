f8e9fecef02e092a1d80a6f91bd9d8e30bb2c6cb
Mert BKatal <mertbk2024@gmail.com>
2025-05-21 11:29:00 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+# Load CBC configuration before starting Plumber
+Rscript -e "source('/app/cbc_config.R')"
 Rscript -e "pr <- plumber::plumb('plumber.R'); pr\$run(host='0.0.0.0', port=as.numeric(Sys.getenv('PORT', 10000)))"
